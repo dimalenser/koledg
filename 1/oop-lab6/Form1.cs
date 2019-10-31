cb574a2cbe34ae0cdc3607b66935a3c5d09b1793
dimalenser <dima.neuron@gmail.com>
2019-10-31 15:35:19 +0200

    папки з проектави імпортую
    імпортую

---
+++ 1/oop-lab6/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop_lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double shuruna = Double.Parse(textBox1.Text);
+            double vusota = Double.Parse(textBox2.Text);
+            double kvmetr = shuruna * vusota;
+            double vartist = 0;
+            if(comboBox1.SelectedIndex==0 && radioButton1.Checked)
+            {
+                vartist = kvmetr * 0.25;
+            }
+            //-----------------------------
+            if (comboBox1.SelectedIndex == 0 && radioButton2.Checked)
+            {
+                vartist = kvmetr * 0.3;
+            }
+            //-----------------------------
+            if (comboBox1.SelectedIndex == 1 && radioButton1.Checked)
+            {
+                vartist = kvmetr * 0.05;
+            }
+            //-----------------------------
+            if (comboBox1.SelectedIndex == 1 && radioButton2.Checked)
+            {
+                vartist = kvmetr * 0.10;
+            }
+            //-----------------------------
+            if (comboBox1.SelectedIndex == 2 && radioButton1.Checked)
+            {
+                vartist = kvmetr * 0.15;
+            }
+            //-----------------------------
+            if (comboBox1.SelectedIndex == 2 && radioButton2.Checked)
+            {
+                vartist = kvmetr * 0.20;
+            }
+            //******************************
+            if(checkBox1.Checked)
+            {
+                vartist += 35;
+            }
+            label_vartist.Text = $"Вартість : {vartist} грн.";
+        }
+    }
+}
---
+++ 2/oop-lab6/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop_lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            this.Text = "Замовлення путівок";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double potivki = Double.Parse(textBox1.Text);
+            double dni = Double.Parse(textBox2.Text);
+            double vartist = 0;
+            if(comboBox1.SelectedIndex==0 && radioButton1.Checked)
+            {
+                vartist =  dni * 100;
+            }
+            //-----------------------------
+            if (comboBox1.SelectedIndex == 0 && radioButton2.Checked)
+            {
+                vartist = dni * 150;
+            }
+            //-----------------------------
+            if (comboBox1.SelectedIndex == 1 && radioButton1.Checked)
+            {
+                vartist = dni * 160;
+            }
+            //-----------------------------
+            if (comboBox1.SelectedIndex == 1 && radioButton2.Checked)
+            {
+                vartist = dni * 200;
+            }
+            //-----------------------------
+            if (comboBox1.SelectedIndex == 2 && radioButton1.Checked)
+            {
+                vartist = dni * 120;
+            }
+            //-----------------------------
+            if (comboBox1.SelectedIndex == 2 && radioButton2.Checked)
+            {
+                vartist = dni * 180;
+            }
+            //******************************
+            vartist *= potivki; //множу вартість однієї потівки на кількість потівок
+            if(checkBox1.Checked)
+            {
+                vartist += dni*50;
+            }
+            label_vartist.Text = $"Вартість путівки: ${vartist}";
+        }
+    }
+}
