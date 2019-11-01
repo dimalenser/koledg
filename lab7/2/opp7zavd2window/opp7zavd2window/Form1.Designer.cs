76fe9d94d9a8d237de920937b83d98cd3ae88609
dimalenser <dima.neuron@gmail.com>
2019-11-01 14:39:30 +0200

    1
    1

--- lab7/2/opp7zavd2window/opp7zavd2window/Form1.Designer.cs
+++ lab7/2/opp7zavd2window/opp7zavd2window/Form1.Designer.cs
@@ -42,6 +42,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(359, 330);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             // 
             // button1
             // 
@@ -51,6 +52,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Промалювати";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -60,6 +62,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Стерти";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
--- lab7/2/opp7zavd2window/opp7zavd2window/Form1.cs
+++ lab7/2/opp7zavd2window/opp7zavd2window/Form1.cs
@@ -10,11 +10,34 @@ using System.Windows.Forms;
 
 namespace opp7zavd2window
 {
+   
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+        public class a { }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            Pen p = new Pen(Color.Blue, 5);// цвет линии и ширина
+            Point p1 = new Point(5, 10);// первая точка
+            Point p2 = new Point(40, 100);// вторая точка
+            gr.DrawLine(p, p1, p2);// рисуем линию
+            gr.Dispose();// освобождаем все ресурсы, связанные с отрисовкой
         }
     }
 }
