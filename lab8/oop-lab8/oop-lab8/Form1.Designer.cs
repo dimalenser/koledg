3fca725a57a1a42d120013ab8a9fc24bcebfc238
dimalenser <dima.neuron@gmail.com>
2019-11-06 14:23:36 +0200

    майже зробив

--- lab8/oop-lab8/oop-lab8/Form1.Designer.cs
+++ lab8/oop-lab8/oop-lab8/Form1.Designer.cs
@@ -28,28 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(33, 85);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(523, 247);
+            this.pictureBox1.Size = new System.Drawing.Size(785, 426);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::oop_lab8.Properties.Resources.rivnist;
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(-8, -3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(153, 120);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(289, 49);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1051, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +102,8 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- lab8/oop-lab8/oop-lab8/Form1.cs
+++ lab8/oop-lab8/oop-lab8/Form1.cs
@@ -16,5 +16,104 @@ namespace oop_lab8
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        void one()
+        {
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.White);
+            Pen a = new Pen(Color.Blue, 1);
+            Pen b = new Pen(Color.Green, 2);
+            Font drawFont = new Font("Arial", 12);
+            Font signatureFont = new Font("Arial", 7);
+
+            SolidBrush drawBrush = new SolidBrush(Color.Blue);
+            StringFormat drawFormat = new StringFormat();
+            drawFormat.FormatFlags = StringFormatFlags.DirectionRightToLeft;
+            int sizeWidth = Form1.ActiveForm.Width;
+            int sizeHeight = Form1.ActiveForm.Height;
+
+            Point center = new Point(((int)(sizeWidth/2)-8), (int)((sizeHeight / 2) - 19));
+
+            g.DrawLine(a, 10, center.Y, center.X, center.Y);
+            g.DrawLine(a, center.X, center.Y, 2 * center.X - 10, center.Y);
+            g.DrawLine(a, center.X, 10, center.X, center.Y);
+            g.DrawLine(a, center.X, center.Y, center.X, 2 * center.Y -10);
+            g.DrawString("X", drawFont, drawBrush, new PointF(2 * center.X - 5, center.Y + 10), drawFormat);
+            g.DrawString("Y", drawFont, drawBrush, new PointF(center.X + 30, 5), drawFormat);
+
+
+            g.DrawLine(a, center.X, 10, center.X + 5, 20);
+            g.DrawLine(a, center.X, 10, center.X - 5, 20);
+
+
+
+            int stepForAxes = 25;
+            int lenghtShtrih = 3;
+            int maxValueForAxesX = 4;
+            int maxValueForAxesY = 9;
+
+            float oneDelenieX = (float)maxValueForAxesX / ((float)center.X / (float)stepForAxes);
+
+            float oneDelenieY = (float)maxValueForAxesY / ((float)center.Y / (float)stepForAxes);
+
+            for (int i = center.X, j = center.X, k = 1; i < 2 * center.X - 30; j -= stepForAxes, i+= stepForAxes, k++)
+            {
+                g.DrawLine(a, i, center.Y - lenghtShtrih, i, center.Y + lenghtShtrih);
+                g.DrawLine(a, j, center.Y - lenghtShtrih, j, center.Y + lenghtShtrih);
+
+                if(i < 2 * center.X - 55)
+                {
+                    g.DrawString((k * oneDelenieX).ToString("0.0"), signatureFont, drawBrush,
+                            new PointF(i + stepForAxes + 9, center.Y + 6), drawFormat);
+                    g.DrawString(((k * oneDelenieX).ToString("0.0").ToString()+ "-"), 
+                        signatureFont, drawBrush, new PointF(j - stepForAxes + 9, center.Y + 6), drawFormat);
+                }
+            }
+
+
+            int numOfPoint = 100;
+
+            float[] first = new float[numOfPoint];
+
+            for(int i = 0; i < numOfPoint; i++)
+            {
+                first[i] = (float)maxValueForAxesX / (float)numOfPoint * (i + 1) - (float)(maxValueForAxesX / 2);
+            }
+
+            float[] second = new float[numOfPoint]; 
+            for(int i = 0; i< numOfPoint; i++)
+            {
+
+            }
+
+            Point[] pointOne = new Point[numOfPoint];
+
+            float tempX = 1 / oneDelenieX * stepForAxes;
+            float tempY = 1 / oneDelenieY * stepForAxes;
+
+            for(int i = 0; i< numOfPoint; i++)
+            {
+                pointOne[i].X = center.X + (int)(first[i] * tempX);
+                pointOne[i].Y = center.Y + (int)(second[i] * tempY);
+            }
+
+            g.DrawLines(b, pointOne);
+            g.DrawCurve(b, pointOne);
+        }
     }
 }
